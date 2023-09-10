f89bbeeb3a35c85cb80455015e18d82ae02186d5
Kartik Maheshwari <95483381+Kartik-Maheshwari@users.noreply.github.com>
2023-09-10 14:31:56 +0530

    Updated Image Path

--- src/App.jsx
+++ src/App.jsx
@@ -36,33 +36,33 @@ export default function App() {
         />
 
         <ParallaxLayer offset={1.3} speed={-0.3} style={{ pointerEvents: 'none' }}>
-          <img src={url('satellite4')} style={{ width: '15%', marginLeft: '70%' }} />
+          <img src='../images/vscode.png' style={{ width: '15%', marginLeft: '70%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '55%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '15%' }} />
+          <img src='../images/cloud.svg' style={{ display: 'block', width: '20%', marginLeft: '55%' }} />
+          <img src='../images/vscode.png' style={{ display: 'block', width: '10%', marginLeft: '15%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '70%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '40%' }} />
+          <img src='../images/cloud.svg' style={{ display: 'block', width: '20%', marginLeft: '70%' }} />
+          <img src='../images/cloud.svg' style={{ display: 'block', width: '20%', marginLeft: '40%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '10%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '75%' }} />
+          <img src='../images/cloud.svg' style={{ display: 'block', width: '10%', marginLeft: '10%' }} />
+          <img src='../images/cloud.svg' style={{ display: 'block', width: '20%', marginLeft: '75%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '60%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '25%', marginLeft: '30%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '10%', marginLeft: '80%' }} />
+          <img src='../images/cloud.svg' style={{ display: 'block', width: '20%', marginLeft: '60%' }} />
+          <img src='../images/cloud.svg' style={{ display: 'block', width: '25%', marginLeft: '30%' }} />
+          <img src='../images/cloud.svg' style={{ display: 'block', width: '10%', marginLeft: '80%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.6 }}>
-          <img src={url('cloud')} style={{ display: 'block', width: '20%', marginLeft: '5%' }} />
-          <img src={url('cloud')} style={{ display: 'block', width: '15%', marginLeft: '75%' }} />
+          <img src='../images/cloud.svg' style={{ display: 'block', width: '20%', marginLeft: '5%' }} />
+          <img src='../images/cloud.svg' style={{ display: 'block', width: '15%', marginLeft: '75%' }} />
         </ParallaxLayer>
 
         <ParallaxLayer
